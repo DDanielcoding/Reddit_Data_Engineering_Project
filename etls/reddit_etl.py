a671ea17a8107966565a242f2018129562daf850
DDanielcoding <draskoczi.daniel@gmail.com>
2024-05-11 11:49:15 +0200

    Initial Commit: Reddit_Data_Engineering_Project: Added reddit_etl, reddit_pipeline

---
+++ etls/reddit_etl.py
@@ -0,0 +1,13 @@
+import sys
+import praw
+from praw import Reddit
+def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
+    try:
+        reddit = praw.Reddit(client_id=client_id,
+                             client_secret=client_secret,
+                             user_agent=user_agent)
+        print("connected to reddit!")
+        return reddit
+    except Exception as e:
+        print(e)
+        sys.exit(1)
--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -1,4 +1,5 @@
-
+from etls.reddit_etl import connect_reddit
+from utils.constants import CLIENT_ID, SECRET
 def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=None):
     #connecting to reddit instance
     instance = connect_reddit(CLIENT_ID, SECRET, 'Airscholar Agent')
